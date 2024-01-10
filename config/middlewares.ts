cc6fa323b6cc2c529af0956e6517b9b6f7e6d9e6
Cem Karakurt <ickarakurt@gmail.com>
2024-01-10 21:15:56 +0100

    Shelf.dev API start

---
+++ config/middlewares.ts
@@ -0,0 +1,34 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+    {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'shelf-dev-staging.s3.eu-central-1.amazonaws.com', "market-assets.strapi.io"],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'shelf-dev-staging.s3.eu-central-1.amazonaws.com', "market-assets.strapi.io"
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
