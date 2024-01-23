861eb8e43f7cdf81484112d22415826ccdcba197
Cem Karakurt <ickarakurt@gmail.com>
2024-01-23 22:28:52 +0100

    add openlibrary api

--- src/api/openlibrary/controllers/openlibrary.ts
+++ src/api/openlibrary/controllers/openlibrary.ts
@@ -8,9 +8,13 @@ export default {
       const data = await strapi
         .service("api::openlibrary.openlibrary")
         .fetchBook();
-      console.log(data, "data");
 
-      ctx.body = data;
+      const authorData = await strapi
+        .service("api::openlibrary.openlibrary")
+        .fetchAuthor("OL2653686A");
+      console.log(authorData, "data");
+
+      ctx.body = authorData;
     } catch (err) {
       ctx.badRequest("Fetch book controller error", { moreDetails: err });
     }
--- src/api/openlibrary/services/openlibrary.ts
+++ src/api/openlibrary/services/openlibrary.ts
@@ -2,10 +2,44 @@
  * openlibrary service
  */
 
+const authorPath = (olId: string) => `https://openlibrary.org/authors/${olId}.json`;
+const authorCoverPath = (olId: string) => `https://covers.openlibrary.org/a/olid/${olId}.jpg`;
+const bookPath = (olId: string) => `https://openlibrary.org/books/${olId}.json`;
+const workPath = (olId: string) => `https://openlibrary.org/works/${olId}.json`;
+const getByIsbnPath = (isbn: string) =>  `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`;
+const editionsPath = (olId: string) => `https://openlibrary.org/works/${olId}/editions.json`;
+
 export default () => ({
-  fetchBook: async (isbn: string) => {
-    return Promise.resolve({
-      title: "The Great Gatsby",
-    });
-  }
+  fetchBook: async (olId: string) => {
+    const response = await fetch(bookPath(olId));
+    const data: any = await response.json();
+    return data;
+  },
+  fetchAuthor: async (olId: string) => {
+    const response = await fetch(authorPath(olId));
+    const data: any = await response.json();
+
+    const returnData = {
+      name: data.name,
+      bio: data.bio.value,
+      birthDate: data.birth_date,
+      cover: authorCoverPath(olId),
+    };
+    return returnData;
+  },
+  fetchWork: async (olId: string) => {
+    const response = await fetch(workPath(olId));
+    const data: any = await response.json();
+    return data;
+  },
+  getByIsbn: async (isbn: string) => {
+    const response = await fetch(getByIsbnPath(isbn));
+    const data: any = await response.json();
+    return data;
+  },
+  fetchEditions: async (olId: string) => {
+    const response = await fetch(editionsPath(olId));
+    const data: any = await response.json();
+    return data;
+  },
 });
