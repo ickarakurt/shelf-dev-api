acc6cae80261dbcd2632a7b41e4f4243c1cc484d
Cem Karakurt <ickarakurt@gmail.com>
2024-03-12 21:23:58 +0100

    fix: remove url

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
