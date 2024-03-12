9fab21a1c9ee8c3e508274187c92522e71fd9194
Cem Karakurt <ickarakurt@gmail.com>
2024-03-12 21:19:06 +0100

    fix: update host

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env('HOST', 'localhost'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('URL', 'http://localhost:1337'),
   app: {
