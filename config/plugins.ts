78231662637bcaf7fe2fa25f218685c5fb180e40
Cem Karakurt <ickarakurt@gmail.com>
2024-05-28 22:32:27 +0200

    fix: commit to retrigger deployment

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,12 +3,14 @@
             config: {
               provider: 'aws-s3',
               providerOptions: {
+                baseUrl: env('AWS_BASE_URL'),
+                rootPath: env('AWS_BASE_URL'),
                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
                 secretAccessKey: env('AWS_ACCESS_SECRET'),
                 region: env('AWS_REGION'),
+                cloudfrontURL: env('AWS_BASE_URL'),
                 params: {
                   ACL: env('AWS_ACL', 'public-read'),
-                  signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
                   Bucket: env('AWS_BUCKET'),
                 },
               },
